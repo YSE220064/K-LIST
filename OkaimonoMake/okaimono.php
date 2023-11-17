a9d37eb0194aa1ab792154545a4c71f95f00d7f7
220064@YSE <220064md@yse-c.net>
2023-11-17 14:31:55 +0900

    save to db

--- OkaimonoMake/okaimono.php
+++ OkaimonoMake/okaimono.php
@@ -25,7 +25,7 @@
             <button id="copy-button" onclick="copyLink()">リスト　リンク　コピー</button>
             <br>
             <button onclick="copyLinkAndGenerateQRCode()">QR　コード　作成</button>
-            <!-- <button onclick="savetodb()">データーベースに保存</button> -->
+            <button onclick="savetodb()">データーベースに保存</button>
             </p>
 
         </div>
