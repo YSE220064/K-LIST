c71718c46f296f097e08ac5c1b146d3c502a06af
220064@YSE <220064md@yse-c.net>
2023-12-04 11:28:05 +0900

    about

---
+++ About/about_backup.php
@@ -0,0 +1,89 @@
+<!-- about.php -->
+<!DOCTYPE html>
+<html lang="en jp">
+<?php include '../WebComponents/head.php'; ?>
+<body>
+    <?php include '../WebComponents/navigation.php'; ?>  
+    <div class="container">
+    <p align="center">
+      <a href="../index.php" target="">
+        <img src="../WebResources/K-List.png" width="200" alt="K-List"></a></p>
+        <h1 class="home_title">K-List</h1>
+      
+        <header>
+        <h1>K-List - 学校卒業プロジェクトについて</h1>
+    </header>
+
+    <section>
+        <h2>プロジェクト概要</h2>
+
+        <p>K-Listは、学校卒業プロジェクトとして誕生した革新的で効果的なアプローチを取ったプロジェクトです。[分野やトピック]において現れた複雑な課題に対する独自の解決策を提供します。</p>
+    </section>
+
+    <section>
+        <h2>ミッションとビジョン</h2>
+
+        <p><strong>ミッション：</strong> K-Listのミッションは、[プロジェクトの主要な目標や目的]を達成し、[影響や変革]をもたらすことです。</p>
+
+        <p><strong>ビジョン：</strong> 私たちは、[プロジェクトが実現しようとする肯定的な未来の姿]を見据えています。</p>
+    </section>
+
+    <section>
+        <h2>プロジェクトの目標</h2>
+
+        <ol>
+            <li><strong>[目標1]：</strong> [第1の目標とその重要性]</li>
+            <li><strong>[目標2]：</strong> [第2の目標とその重要性]</li>
+            <li><strong>[目標3]：</strong> [第3の目標とその重要性]</li>
+        </ol>
+    </section>
+
+    <section>
+        <h2>主な特徴</h2>
+
+        <ul>
+            <li><strong>革新的なアプローチ：</strong> K-Listは、[プロジェクトで採用した独自の手法や技術についての説明]を特徴としています。</li>
+            <li><strong>協力の精神：</strong> 私たちは[協力者やメンター]と協力し、[外部のパートナー]との連携を重視しています。</li>
+            <li><strong>持続可能性：</strong> 環境への責任を果たすため、K-Listは[具体的な環境に優しい取り組み]を積極的に取り入れています。</li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>チームメンバー</h2>
+
+        <ul>
+            <li><strong>[チームメンバー1]：</strong> [役割と簡単な経歴]</li>
+            <li><strong>[チームメンバー2]：</strong> [役割と簡単な経歴]</li>
+            <li><strong>[チームメンバー3]：</strong> [役割と簡単な経歴]</li>
+            <li><strong>[チームメンバー4]：</strong> [役割と簡単な経歴]</li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>謝辞</h2>
+
+        <p>私たちはこのプロジェクトの進行にあたり、サポートしてくださった個人や組織に深く感謝しています。特に以下の方々に感謝の意を表します：</p>
+
+        <ul>
+            <li>[名前や組織名] - [具体的なサポートや貢献についての説明]</li>
+            <li>[名前や組織名] - [具体的なサポートや貢献についての説明]</li>
+            <li>[名前や組織名] - [具体的なサポートや貢献についての説明]</li>
+        </ul>
+
+        <p>彼らの協力により、私たちはより優れた結果を達成し、K-Listプロジェクトを成功に導くことができました。深く感謝申し上げます。</p>
+    </section>
+
+    <section>
+        <h2>お問い合わせ</h2>
+
+        <p>質問やフィードバックがありましたら、お気軽に [あなたのメールアドレス] までお問い合わせください。</p>
+    </section>
+
+    <footer>
+        <p>「About」ページを訪れていただき、K-Listプロジェクトが皆さんにとってどれだけインスパイアを与えるものであるかを願っています！</p>
+    </footer>
+      
+    </div>
+    <?php include '../WebComponents/footer.php'; ?>
+</body>
+</html>
