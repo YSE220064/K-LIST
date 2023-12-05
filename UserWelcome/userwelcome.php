3c5af290419341f125186d3bc32f9f244f127765
220064@YSE <220064md@yse-c.net>
2023-12-05 11:27:21 +0900

    new css

--- UserWelcome/userwelcome.php
+++ UserWelcome/userwelcome.php
@@ -7,23 +7,30 @@
     <div class="container">
     <p align="center">
       <a href="../index.php" target="">
-        <img src="../WebResources/K-List.png" width="200" alt="K-List"></a></p>
-        <h1 class="home_title">K-List</h1>
-        <h3 class="home_subtitle">お買い物リスト</h3>
+      <img src="../WebResources/K-List-Logo.jpg" width="200" alt="K-List"></a></p>
+        <!-- <h1 class="home_title">K-List</h1> -->
+        <!-- <h3 class="home_subtitle">お買い物リスト</h3> -->
       
         <div class="user_registration">
-        <a href="../UserRegistration/userregistration.php">
-          <button type="submit">アカウント　作成</button></a>
-      </div>
+        <!-- <a href="../UserRegistration/userregistration.php" >
+          <button type="submit">アカウント　作成</button></a> -->
+          <a href="../UserRegistration/userregistration.php" class="btn btn-flat"><span>アカウント　作成</span></a>
+      </div><br>
       
       <div class="user_login">
-        <a href="../UserLogin/userlogin.php">
-          <button type="submit">アカウント　ログイン</button></a>
-      </div>
+        <!-- <a href="../UserLogin/userlogin.php"> -->
+          <!-- <button type="submit">アカウント　ログイン</button></a> -->
+          <a href="../UserLogin/userlogin.php" class="btn btn-flat"><span>アカウント　ログイン</span></a>
+      </div><br>
 
-      <div class="okaimono_make">
+      <!-- <div class="okaimono_make">
         <a href="../OkaimonoMake/okaimono.php">
           <button type="submit">リスト　作る</button></a>
+          <a href="../OkaimonoMake/okaimono.php" class="btn btn-flat"><span>リストを作る</span></a>
+      </div><br> -->
+
+      <div class="okaimono_make">
+        <a href="../OkaimonoMake/okaimono.php" class="btn btn-flat"><span>リストを作る</span></a>
       </div>
       
     </div>
