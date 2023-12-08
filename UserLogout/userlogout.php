2c3cd8c104608b6fb0abf767559e4eefaed107ae
220064@YSE <220064md@yse-c.net>
2023-12-08 13:17:51 +0900

    userlogoutsessions

--- UserLogout/userlogout.php
+++ UserLogout/userlogout.php
@@ -1,4 +1,29 @@
 <!-- userlogout.php -->
+<?php
+// Start the session
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['user_id'])) {
+    // Redirect to the login page if the user is not logged in
+    header("Location: ../UserLogin/userlogin.php");
+    exit;
+}
+
+// Handle logout
+if (isset($_POST['logout'])) {
+    // Unset all session variables
+    $_SESSION = array();
+
+    // Destroy the session
+    session_destroy();
+
+    // Redirect to the login page
+    header("Location: ../UserLogin/userlogin.php");
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en jp">
 
@@ -12,9 +37,18 @@
 
     <div class="container">
         <img src="../WebResources/K-List-Logo.jpg" width="200" alt="K-List"></a></p>
-        <h1>ログアウト
-            <br>未定
-        </h1>
+        <header>
+        <h1>Test <?php echo $_SESSION['username']; ?></h1>
+    </header>
+
+    <!-- <section>
+        <p>This is a simple web page created with PHP.</p>
+        <p>Current Date: <?php echo date("Y-m-d H:i:s"); ?></p>
+    </section> -->
+
+    <form method="post">
+        <input type="submit" name="logout" value="Logout">
+    </form>
     </div>
     <br>
 
